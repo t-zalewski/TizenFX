b3d174a0d9ad1583b2f6f9307f1d431af630dd8a
Tomasz Zalewski <t.zalewski@samsung.com>
2019-02-08 13:28:04 +0100

    Add shell class

---
+++ src/Tizen.WindowManager/Tizen.WindowManager/Tizen.WindowManager/SafeQuickpanelHandle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Tizen.WindowManager
+{
+    internal class SafeQuickpanelHandle : SafeHandle
+    {
+        internal SafeQuickpanelHandle() : base(IntPtr.Zero, true)
+        {
+        }
+
+        internal SafeQuickpanelHandle(IntPtr existingHandle, bool ownHandle) : base(IntPtr.Zero, ownHandle)
+        {
+            SetHandle(existingHandle);
+        }
+
+        public override bool IsInvalid
+        {
+            get { return this.handle == IntPtr.Zero; }
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            var ret = Interop.Quickpanel.Destroy(this.handle);
+            if (ret != (int)QuickpanelError.None)
+            {
+                Tizen.Log.Error(Interop.Quickpanel.logTag, $"Failed to destroy object: {Internals.Errors.ErrorFacts.GetErrorMessage(ret)}");
+            }
+            this.SetHandle(IntPtr.Zero);
+            return true;
+        }
+    }
+}
---
+++ src/Tizen.WindowManager/Tizen.WindowManager/Tizen.WindowManager/Shell.cs
@@ -0,0 +1,84 @@
+﻿using ElmSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tizen.WindowManager
+{
+    enum ShellType
+    {
+        Unknown = 0,
+        EFL = 1
+    }
+
+    /// <summary>
+    /// Class 
+    /// </summary>
+    /// <since_tizen> 6 </since_tizen>
+    public class Shell : IDisposable
+    {
+        internal SafeShellHandle shellHandler;
+        internal Int32 windowId;
+
+        private bool _isDisposed;
+
+        ///<summary>
+        /// Initializes a new instance of the <see cref="Shell"/> class.
+        /// </summary>
+        /// <param name="parent">Parent EvasObject</param>
+        /// <since_tizen> 6 </since_tizen>
+        public Shell(EvasObject parent)
+        {
+            shellHandler = Interop.WindowManagerShell.Create((int)ShellType.EFL);
+
+            if (shellHandler == null)
+            {
+                var err = Internals.Errors.ErrorFacts.GetLastResult();
+                if (err != (int)QuickpanelError.None)
+                {
+                    throw QuickpanelErrorFactory.CheckAndThrowException(err, "Unable to create Quickpanel");
+                }
+            }
+
+            windowId = Interop.WindowManagerShell.GetElmWindowId(parent);
+        }
+
+        /// <summary>
+        /// Releases any unmanaged resources used by this object.
+        /// </summary>
+        /// <since_tizen> 6 </since_tizen>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Destroy the Shell object
+        /// </summary>
+        /// <since_tizen> 6 </since_tizen>
+        ~Shell()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Releases any unmanaged resources used by this object. Can also dispose any other disposable objects.
+        /// </summary>
+        /// <param name="disposing"></param>
+        /// <since_tizen> 6 </since_tizen>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    if (shellHandler != null && !shellHandler.IsInvalid)
+                        shellHandler.Dispose();
+                }
+
+                _isDisposed = true;
+            }
+        }
+    }
+}
